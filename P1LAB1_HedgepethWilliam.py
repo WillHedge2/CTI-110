aba6d74eb6403d1e243b191ed8638988887eda15
WillHedge2 <hedgepew6150@student.faytechcc.edu>
2025-02-06 11:27:25 -0500

    Add files via upload

---
+++ P1LAB1_HedgepethWilliam.py
@@ -0,0 +1,11 @@
+# William Hedgepeth
+# 02/04/2025
+# P1LAB1
+# Using the input ad print functions to display a name
+
+# Get user input for first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display welcome message using first and last name
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
